a2e7733abd7fe7849fb977d9f10dc5073d64561e
23080ad <23080ad@hvhs.school.nz>
2026-03-03 12:30:17 +1300

    player movemnet + developing enemyb spawning counter

--- game_script.js
+++ game_script.js
@@ -1,34 +1,43 @@
 /*******************************************************/
 // game Play
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 
-//variables
+//VARIABLES
 
-var width;
-var height;
-var pos_x;
-var pos_y;
+//world setup variables
+var gameSpeed = 60;
+var 
 
+//player movement variables
+var playerVel = 5;
+var playerScale = 40;
+
+
+
+
+//Enemy spawning variables
+
+//var randomYvalue = random
 
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-	
+
+	//set game speed
+	frameRate(gameSpeed);
+
 	//draw the canvas
-	cnv = new Canvas(windowWidth, windowHeight);
+	cnv = new Canvas(windowWidth - 20, windowHeight - 20);
 
-	
-	//create a rectangle
-	rectangle = new Sprite(100, 100, 20, 20, 10);
-	rectangle.color = 'green';
+	//create the player charcter
+	player = new Sprite(windowWidth / 2, windowHeight / 2, playerScale, playerScale);
 
-	//create a circle
-	circle = new Sprite(200, 200, 10, 10)
 }
-	
+
+
 /*******************************************************/
 // draw()
 /*******************************************************/
@@ -36,8 +45,60 @@ function draw() {
 
 	//color the bg
 	background('grey');
-}
 
+
+	//player movement script
+
+	//LEFT
+	if (kb.pressing('left')) {
+		player.vel.x = playerVel * -1;
+	}
+	else if (kb.released('left')) {
+		player.vel.x = 0;
+	}
+
+	//RIGHT
+	if (kb.pressing('right')) {
+		player.vel.x = playerVel;
+	}
+	else if (kb.released('right')) {
+		player.vel.x = 0;
+	}
+
+	//UP
+	if (kb.pressing('up')) {
+		player.vel.y = playerVel * -1;
+	}
+	else if (kb.released('up')) {
+		player.vel.y = 0;
+	}
+
+	//DOWN
+	if (kb.pressing('down')) {
+		player.vel.y = playerVel;
+	}
+	else if (kb.released('down')) {
+		player.vel.y = 0;
+	}
+
+	//Enemy spawning
+
+
+	//spawn timer
+
+
+
+	//for (i = 0; i < 1000; i++) {
+
+		//enemy = new Sprite(random(0, windowWidth), random(0, windowHeight), 20, 20);
+
+		//enemy.vel.x = 3;
+
+		//enemy.vel.y = 4;
+	//}
+
+	/****************************** */
+}
 /*******************************************************/
 //  END of code
 /*******************************************************/
