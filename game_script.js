a03ad64a121b844d9959ef97a89ca194763aa656
23080ad <23080ad@hvhs.school.nz>
2026-03-17 23:30:02 +1300

    pause fully implemented on in game objects and timer

--- game_script.js
+++ game_script.js
@@ -9,6 +9,13 @@
 
 //world setup variables
 var gameSpeed = 60;
+var clockPause = 0;
+
+//pause the game
+let pauseRun = false;
+
+//side bar formatting
+var sideBarWidth = 250;
 
 //roundPlay indicates that the game is currently in a round. Meanwhile,
 //roundEnd indicates that a round has just finished. this does NOT include the time spent on the start screen before a round even starts.
@@ -49,6 +56,7 @@ var rockNumber = -1;
 //score variables
 var finalScore = 0;
 var highScore = 0;
+var instantPause = 0;
 
 
 /*******************************************************/
@@ -61,10 +69,10 @@ function setup() {
 
     //set game speed
     frameRate(gameSpeed);
-    
+
     //draw the canvas
     cnv = new Canvas(windowWidth, windowHeight);
-    
+
     resizeCanvas(windowWidth, windowHeight);
 
     //create the ENEMY and ROCK sprite groups
@@ -77,10 +85,15 @@ function setup() {
     }
 
     //create the world borders L, R, U, D.
-    BorderL = new Sprite(0, windowHeight/2, 1, windowHeight, 's');
-    BorderR = new Sprite(windowWidth, windowHeight/2, 1, windowHeight, 's');
-    BorderU = new Sprite(windowWidth/2, windowHeight, windowWidth, 1, 's');
-    BorderD = new Sprite(windowWidth/2, 0, windowWidth, 1, 's');
+    BorderL = new Sprite(0, windowHeight / 2, 1, windowHeight, 's');
+    BorderR = new Sprite(windowWidth - sideBarWidth, windowHeight / 2, 1, windowHeight, 's');
+    BorderU = new Sprite(windowWidth / 2, windowHeight, windowWidth, 1, 's');
+    BorderD = new Sprite(windowWidth / 2, 0, windowWidth, 1, 's');
+
+    //create the side bar
+    Side_Bar = new Sprite(windowWidth - sideBarWidth / 2, windowHeight / 2, sideBarWidth, windowHeight, 'n');
+    Side_Bar.color = 'tan';
+
 
     //create the player charcter
     player = new Sprite(windowWidth / 2, windowHeight / 2, playerScale, playerScale, 'd');
@@ -115,14 +128,14 @@ function draw() {
     //draws all sprites first so that the text object can then be drawn infront of them
     allSprites.draw();
 
-    
+
     //CLOCK
 
     //if clockIsOn is true, the game will calculate the rounded value of how many milliseconds
     //the game has been running for minus how long the game has been running for before the clock was started,
     //divided by 1000 to calculate how many seconds its been running for.
     if (clockIsOn) {
-        clockTime = floor((millis() - clockStartTime) / 1000);
+        clockTime = floor(((millis() - clockStartTime) / 1000) - clockPause );
     }
 
     //draw the Title
@@ -137,16 +150,16 @@ function draw() {
     if (roundPlay) {
 
         // Display the clock on screen
-        fill('white');
+        fill('black');
         textAlign(CENTER);
         textSize(32);
-        text("Time: " + clockTime, width / 2, height / 4);
+        text("Time: " + clockTime, (windowWidth - (sideBarWidth / 2)), height / 4);
 
         // display the players health on screen
-        fill('red');
-        textAlign(RIGHT);
+        fill('black');
+        textAlign(CENTER);
         textSize(32);
-        text("Health: " + playerHealth, width - 100, height / 10);
+        text("Health: " + playerHealth, (windowWidth - (sideBarWidth / 2)), height / 6);
 
         //direct the Hollow Purple
         //hollow_purple.moveTo(mouse, 10);
@@ -162,6 +175,34 @@ function draw() {
         //stop the player from bouncing off rocks and enemies
         rockGroup.collided(player, playerCollidesSolid);
         enemyGroup.collided(player, playerCollidesEnemy);
+
+        //toggles and untoggles the world time scale which freezes time on the physics simulation
+        if (kb.presses('p')) {
+            world.timeScale = !world.timeScale;
+            pauseRun = !pauseRun;
+            clockIsOn = !clockIsOn;
+
+        }
+
+
+        //when  pause is activated, instantPause sets itself to the current clock time. 
+        //Next a second timer starts which sets clockPause's value to however long the round has been running
+        //minus this instant pause to calculate how long the round has been runnig after the pause.
+        //Finally this value is constantly being removed from the clockTime variable in timer
+        //meaning that the time is allways in the right place when it restarts having accounted for any and all pauses.
+        if (pauseRun) {
+            if (instantPause < 1) {
+                instantPause = clockTime;
+                console.log("instant pause time: " + instantPause);
+            }
+            clockPause = floor(((millis() - clockStartTime) / 1000) - instantPause);
+            console.log("clock pause: " + clockPause);
+        }
+        //when the game becomes unpaused this resets instantPause so it can be reset in the above if stament when "instantPause < 1"
+        if (!pauseRun){
+             instantPause = 0;
+        }
+
     }
 
     //ROUND OVER triggers
@@ -173,11 +214,13 @@ function draw() {
         textSize(100);
         text("FINAL SCORE " + finalScore + "!", windowWidth / 2, windowHeight / 2);
     }
+
+    //displays high score
     if (roundEnd) {
         fill('black');
         textAlign(CENTER);
         textSize(50);
-        text("HIGH SCORE " + highScore , windowWidth / 2, (windowHeight / 2) + 100);
+        text("HIGH SCORE " + highScore, windowWidth / 2, (windowHeight / 2) + 100);
     }
 
 }
@@ -189,11 +232,11 @@ function draw() {
 //triggers all events that need to happen when a new round is started.
 //Acts as a start button and a restart button so it must also reset all events from teh round over state.
 function startRound() {
-    
+
     //switches game state to roundPlay and disables all other potential agme states
     roundPlay = true;
     roundEnd = false;
-    
+
 
     //removing all enemies from enemyGroup and deleting them
     enemyGroup.removeAll();
@@ -201,10 +244,10 @@ function startRound() {
 
     //resset player stats & send them to their start location
     playerHealth = 5;
-    
-    player.x = windowWidth/2;
-    player.y = windowHeight/2;
-    
+
+    player.x = windowWidth / 2;
+    player.y = windowHeight / 2;
+
     //create the background rocks
     for (rocks = 0; rocks < random(15, 20); rocks++) {
         createRock();
@@ -228,71 +271,77 @@ function startRound() {
 /*******************************************************/
 function enemySpawning() {
 
-    //make spawned enemies move towards the player by targeting their group
-    for (let enemy of enemyGroup) {
-        // Use rotateTo to instantly face the target
-        enemy.rotateTowards(player, 0.05);
-        enemy.moveTo(player, 1)
-    }
+    //if the game isnt paused activate the following, otherwise stop enemies from spawning
+    if (pauseRun === false) {
 
-    //choose which side of the screen enemies spawn from
-    enemySpawnPositioning = floor(random(1, 4.999999999999999999999999999999999999));
-    //NOTE: since the floor function rounds down a value to the nearest whole number
-    // we must set the range to just higher than the top value we want to be possible.
-    // however it must be as far higher as possible to make sure each options has the largest
-    // number of possible opprtunites to be selected.
-    // Due to the minute differences 4.999 should be reasonable
-
-    //Spawn an enemy every 5/3 seconds
-    if (spawnCounter < 5 / 3 * gameSpeed) {
-        spawnCounter++;
-    }
-    else if (spawnCounter >= 5 / 3 * gameSpeed) {
-
-        // since our frame rate is set to 60fps, the draw loop runs that many times per second.
-        // so to spawn an enemy every five seconds we make the counter tick up to (5/3 x the "gameSpeed" variable)
-        // before spawning an enemy and resetting.
-        //
-        // right now we seem to be able to spawn around 550 enemies on the school before the game begins to slow down.
-        // NOTE: this may only be possible due to the lack of other objects on the screen
-
-        //set enemy spawn coordinates
-
-        //spawns an enemy from the left edge  of screen
-        if (enemySpawnPositioning == 1) {
-            enemyNumber = enemyNumber + 1;
-            enemy = new Sprite(0, random(0, windowHeight), 20, 20);
-
-            enemyGroup.add(enemy);
-            spawnCounter = 0;
+        //make spawned enemies move towards the player by targeting their group
+        for (let enemy of enemyGroup) {
+            // Use rotateTo to instantly face the target
+            enemy.rotateTowards(player, 0.05);
+            enemy.moveTo(player, 1)
         }
-        //spawns an enemy from the right edge  of screen
-        else if (enemySpawnPositioning == 2) {
-            enemyNumber = enemyNumber + 1;
-            enemy = new Sprite(windowWidth, random(0, windowHeight), 20, 20);
 
-            enemyGroup.add(enemy);
-            spawnCounter = 0;
-        }
-        //spawns an enemy from the top edge  of screen
-        else if (enemySpawnPositioning == 3) {
-            enemyNumber = enemyNumber + 1;
-            enemy = new Sprite(random(0, windowWidth), 0, 20, 20);
+        //choose which side of the screen enemies spawn from
+        enemySpawnPositioning = floor(random(1, 4.999999999999999999999999999999999999));
+        //NOTE: since the floor function rounds down a value to the nearest whole number
+        // we must set the range to just higher than the top value we want to be possible.
+        // however it must be as far higher as possible to make sure each options has the largest
+        // number of possible opprtunites to be selected.
+        // Due to the minute differences 4.999 should be reasonable
 
-            enemyGroup.add(enemy);
-            spawnCounter = 0;
+        //Spawn an enemy every 5/3 seconds
+        if (spawnCounter < 5 / 3 * gameSpeed) {
+            spawnCounter++;
         }
-        //spawns an enemy from the bottom edge of screen
-        else if (enemySpawnPositioning == 4) {
-            enemyNumber = enemyNumber + 1;
-            enemy = new Sprite(random(0, windowWidth), windowHeight, 20, 20);
+        else if (spawnCounter >= 5 / 3 * gameSpeed) {
 
-            enemyGroup.add(enemy);
-            spawnCounter = 0;
+            // since our frame rate is set to 60fps, the draw loop runs that many times per second.
+            // so to spawn an enemy every five seconds we make the counter tick up to (5/3 x the "gameSpeed" variable)
+            // before spawning an enemy and resetting.
+            //
+            // right now we seem to be able to spawn around 550 enemies on the school before the game begins to slow down.
+            // NOTE: this may only be possible due to the lack of other objects on the screen
+
+            //set enemy spawn coordinates
+
+            //spawns an enemy from the left edge  of screen
+            if (enemySpawnPositioning == 1) {
+                enemyNumber = enemyNumber + 1;
+                enemy = new Sprite(0, random(0, windowHeight), 20, 20);
+
+                enemyGroup.add(enemy);
+                spawnCounter = 0;
+            }
+            //spawns an enemy from the right edge  of screen
+            else if (enemySpawnPositioning == 2) {
+                enemyNumber = enemyNumber + 1;
+                //this is so enemies dont spawn behind or in the sidebar
+                //which is why we have both the minus side bar width and the minus 5 for thoss reasons in that order.
+                enemy = new Sprite((windowWidth - sideBarWidth) - 5, random(0, windowHeight), 20, 20);
+
+                enemyGroup.add(enemy);
+                spawnCounter = 0;
+            }
+            //spawns an enemy from the top edge  of screen
+            else if (enemySpawnPositioning == 3) {
+                enemyNumber = enemyNumber + 1;
+                enemy = new Sprite(random(0, (windowWidth - sideBarWidth) - 5), 0, 20, 20);
+
+                enemyGroup.add(enemy);
+                spawnCounter = 0;
+            }
+            //spawns an enemy from the bottom edge of screen
+            else if (enemySpawnPositioning == 4) {
+                enemyNumber = enemyNumber + 1;
+                enemy = new Sprite(random(0, (windowWidth - sideBarWidth) - 5), windowHeight, 20, 20);
+
+                enemyGroup.add(enemy);
+                spawnCounter = 0;
+            }
+            console.log(enemyGroup);
+            console.log("Enemy Spawned From Edge " + enemySpawnPositioning);
+            console.log("Enemy Number " + enemyNumber);
         }
-        console.log(enemyGroup);
-        console.log("Enemy Spawned From Edge " + enemySpawnPositioning);
-        console.log("Enemy Number " + enemyNumber);
     }
 }
 
@@ -334,6 +383,7 @@ function playerMovement() {
     else if (kb.released('down')) {
         player.vel.y = 0;
     }
+
 }
 
 
@@ -391,7 +441,7 @@ function roundOver() {
 
     //calculate final score
     finalScore = clockTime;
-    if (finalScore > highScore){
+    if (finalScore > highScore) {
         highScore = finalScore;
     }
     console.log("Final score: " + clockTime);
@@ -415,7 +465,7 @@ function roundOver() {
 
 //create a rock
 function createRock() {
-    let rock = new Sprite(random(0, windowWidth), random(0, windowHeight), 50, 'k');
+    let rock = new Sprite(random(0, (windowWidth - sideBarWidth) - 5), random(0, windowHeight), 50, 'k');
     rock.mass = 0.1;
     rockNumber = rockNumber + 1;
     rock.name = "rock " + rockNumber;
